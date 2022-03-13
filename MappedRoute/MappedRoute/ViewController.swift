21b5ac7833135ad0577691341fdebb69df3ee764
Hridayedeep Gupta <hridayedeepgupta@gmail.com>
2022-03-13 23:20:43 +0530

    BasicSetup

---
+++ MappedRoute/MappedRoute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MappedRoute
+//
+//  Created by Hridayedeep Gupta on 13/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
